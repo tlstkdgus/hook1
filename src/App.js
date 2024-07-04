70c84e4bc36f1bd55dc5f01576f67268d763ab14
tlstkdgus <tlstkdgus9@naver.com>
2024-07-04 13:57:51 +0900

    react_chapter5_1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { Resume } from "./components/Resume";
+
 function App() {
   return (
-    <div>
-      hello world
-    </div>
+    <>
+      <Resume hello="안녕하세요" name="개리" hobby="게임" food="고기" color="blue"/>
+    </>
   );
 }
 export default App;
---
+++ src/components/Resume.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export function Resume(props){
+
+    const [like, setLike] = useState(0);
+
+    // let like = 0;
+
+    function clickLike() {
+        // like += 1;
+        setLike(like+1);
+        console.log(like);
+    }
+
+    const myColor = props.color;
+    const styleColor = {color:myColor};
+
+    return(
+        <div style={{border:"solid 1px", width:"500px"}}>
+            <h1>{props.name} 자기소개서</h1>
+            <h1>{props.hello}</h1>
+            <h2>취미 : {props.hobby}</h2>
+            <h2>좋아하는 음식 : {props.food}</h2>
+            <h2>좋아하는 색 : <span style={styleColor}>{myColor}</span></h2>
+            <button onClick={clickLike}>like <span>{like}</span></button>
+        </div>
+    )
+}
